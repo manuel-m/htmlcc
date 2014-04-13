bda0baf41afb59dd0cbb299709fd350c21f02e11
manuel-m <discoride.dev@gmail.com>
2014-04-13 17:30:01 +0200

    xxd like branch

---
+++ src/lib/hxd.h
@@ -0,0 +1,22 @@
+#ifndef EMBED_H
+#define	EMBED_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+typedef struct {
+  const size_t sz,
+  const uint8_t* data,
+  const char key[],
+} mmembed_s;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* EMBED_H */
+
---
+++ src/tools/embed.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mmtrace.h"
+#include "diru.h"
+
+#define QR_BUFFER_SIZE (1<<10)
+#define QR_NB_COLS (1<<4)
+
+typedef struct {
+    FILE* fout;
+    size_t nbelem;
+    const char* prefix;    
+} ctx_s;
+
+static int hxd_file(FILE *out_, const char* symname_,const char* filename_){
+    int res = 0;
+    uint8_t icol = 0;
+    size_t sz = 0;
+    
+    if(!out_) MM_GERR("internal out_ NULL");
+    if(!symname_) MM_GERR("internal symname_ NULL");
+    if(!filename_) MM_GERR("internal filename_ NULL");
+
+    FILE *f = fopen(filename_, "rb");
+    if (!f) MM_GERR("read file %s", filename_);
+
+    uint8_t buffer[QR_BUFFER_SIZE];
+    memset(buffer,0,sizeof(buffer));
+
+    
+    /* --> data */
+    fprintf(out_,"static const uint8_t %s_data[] = {\n", symname_);
+        
+    /* --> values */
+    do {
+        const size_t nbelem = fread(buffer, 1, sizeof (buffer), f);
+        size_t i;
+
+        for (i = 0; i < nbelem; i++) {
+            fprintf(out_,"0x%02x,", buffer[i]);
+            ++icol;
+            ++sz;
+            icol &= ~QR_NB_COLS;
+            if (!icol)puts("");
+        }
+        if (sizeof (buffer) != nbelem) {
+            fprintf(out_,"\n");
+            break;
+        }
+    } while (1);
+    
+    /* --> size */
+    fprintf(out_,"};\n");
+    
+    /* --> full struct */
+    fprintf(out_,"static const mmembed_s %s = {\n",symname_);
+    fprintf(out_,"  .size = %zu;\n", sz);
+    fprintf(out_,"  .data = %s_data;\n",symname_);
+    fprintf(out_,"  .key = \"%s\";\n",filename_);
+    fprintf(out_,"};\n");
+    
+
+end:
+    if (f)fclose(f);
+    return res;
+
+err:
+    res = -1;
+    goto end;
+
+}
+
+static int hxd_header(ctx_s* pctx_){
+    int res = 0;
+    
+    if(!pctx_)MM_GERR("internal error");    
+    
+    fprintf(pctx_->fout, "#include \"hxd.h\"\n" );
+   
+
+end:
+    return res;
+
+err:
+    res = -1;
+    goto end;    
+}
+
+static void onfile(const char *path_, const char *basename_, int level_, void* user_data_) {
+    (void) level_;
+    ctx_s* pctx = user_data_;
+    
+    char* fullname = NULL;
+    if(0 > asprintf(&fullname,"%s/%s",path_, basename_)) MM_ERR("internal error");
+    
+    char* symname = NULL;
+    if(0 > asprintf(&symname,"%s_%zu", pctx->prefix,pctx->nbelem )) MM_ERR("internal error");
+    
+    hxd_file(pctx->fout, symname,fullname);
+    
+    ++(pctx->nbelem);
+    
+    if(symname)free(symname);
+    if(fullname)free(fullname);
+}
+
+int main(int argc, char** argv) {
+
+    int res = 0;
+    if (3 != argc) goto bad_cmd;
+    
+        ctx_s ctx = {
+        .fout = stdout,
+        .nbelem = 0,
+        .prefix = argv[2]
+    };
+    
+    hxd_header(&ctx);
+    
+    diru_parse(argv[1], 0, NULL, onfile, &ctx);
+    
+  
+end:
+    return res;
+
+err:
+    res = -1;
+    goto end;
+
+bad_cmd:
+    puts("bad invocation");
+    goto err;
+}
