03c201cacc10210ff99655428a1e611416f2022c
manuel-m <discoride.dev@gmail.com>
2014-05-14 00:06:42 +0200

    webhello use read

--- src/samples/webhello.c
+++ src/samples/webhello.c
@@ -7,48 +7,57 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <err.h>
- 
+
+#include <string.h>
+
 char response[] = "HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
-"<style>body { background-color: #111 }"
-"h1 { font-size:4cm; text-align: center; color: black;"
-" text-shadow: 0 0 2mm red}</style></head>"
-"<body><h1>Goodbye, world!</h1></body></html>\r\n";
- 
-int main()
-{
-  int one = 1, client_fd;
-  struct sockaddr_in svr_addr, cli_addr;
-  socklen_t sin_len = sizeof(cli_addr);
- 
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
-    err(1, "can't open socket");
- 
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
- 
-  int port = 8080;
-  svr_addr.sin_family = AF_INET;
-  svr_addr.sin_addr.s_addr = INADDR_ANY;
-  svr_addr.sin_port = htons(port);
- 
-  if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
-    close(sock);
-    err(1, "Can't bind");
-  }
- 
-  listen(sock, 5);
-  while (1) {
-    client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
-    printf("got connection\n");
- 
-    if (client_fd == -1) {
-      perror("Can't accept");
-      continue;
+        "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+        "<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
+        "<style>body { background-color: #111 }"
+        "h1 { font-size:4cm; text-align: center; color: black;"
+        " text-shadow: 0 0 2mm red}</style></head>"
+        "<body><h1>Goodbye, world!</h1></body></html>\r\n";
+
+int main() {
+    int one = 1, client_fd;
+    struct sockaddr_in svr_addr, cli_addr;
+    socklen_t sin_len = sizeof (cli_addr);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        err(1, "can't open socket");
+
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof (int));
+
+    int port = 8080;
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_addr.s_addr = INADDR_ANY;
+    svr_addr.sin_port = htons(port);
+
+    if (bind(sock, (struct sockaddr *) &svr_addr, sizeof (svr_addr)) == -1) {
+        close(sock);
+        err(1, "Can't bind");
     }
- 
-    write(client_fd, response, sizeof(response) - 1); /*-1:'\0'*/
-    close(client_fd);
-  }
+
+    listen(sock, 5);
+
+    char buffer[2048];
+    memset(buffer, 0, sizeof buffer);
+
+    while (1) {
+        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+
+        if (client_fd == -1) {
+            perror("Can't accept");
+            continue;
+        }
+       
+        int lg = read(client_fd, buffer, sizeof buffer);
+        buffer[lg]='\0';
+        puts(buffer);
+
+        lg = write(client_fd, response, sizeof (response) - 1); /*-1:'\0'*/
+        (void)lg;
+        close(client_fd);
+    }
 }
